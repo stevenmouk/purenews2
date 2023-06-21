ad05cd5b1bee9c0fc5113177afddfe14084948c5
Steven Moukhametzianov <stevenmoukhametzianov@Stevens-MBP.home>
2023-06-21 19:20:29 -0400

    er

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,25 @@
+import Footer from "@/components/Footer";
 import "@/styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-7W1Z8C56G2"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-7W1Z8C56G2');
+        `}
+      </Script>
       <Component {...pageProps} />
-      <footer className="p-4  sm:p-6 bg-white">
-        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center ">
-            © 2023{" "}
-            <a href="https://flowbite.com" className="hover:underline">
-              lots of AI™
-            </a>
-            . All Rights Reserved.
-          </span>
-          <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-            {/* <p className="text-gray-500 dark:hover:text-white mr-24">
-              contact: contact@lotsofai.com
-            </p> */}
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </>
   );
 }
