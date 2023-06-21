8157f74cb35366768a4e2ee5133449c2858c4210
Steven Moukhametzianov <stevenmoukhametzianov@Stevens-MBP.home>
2023-06-21 14:44:51 -0400

    er

--- pages/index.js
+++ pages/index.js
@@ -10,34 +10,34 @@ import Head from "next/head";
 const archivo = Archivo({ subsets: ["latin"], weight: "800" });
 
 export default function Home({ articles }) {
-  // const [articles1, setArticles1] = useState([]);
+  const [articles1, setArticles1] = useState([]);
 
-  // const [articles2, setArticles2] = useState([]);
+  const [articles2, setArticles2] = useState([]);
 
-  // useEffect(() => {
-  //   const formattedArticles = articles?.map((article) => {
-  //     const { img_url, title, timestamp, path } = article;
-  //     const date = new Date(timestamp * 1000);
-  //     const formattedDate = date.toLocaleString("en-US", {
-  //       month: "short",
-  //       day: "numeric",
-  //       hour: "numeric",
-  //       minute: "numeric",
-  //       timeZoneName: "short",
-  //     });
+  useEffect(() => {
+    const formattedArticles = articles.map((article) => {
+      const { img_url, title, timestamp, path } = article;
+      const date = new Date(timestamp * 1000);
+      const formattedDate = date.toLocaleString("en-US", {
+        month: "short",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        timeZoneName: "short",
+      });
 
-  //     return {
-  //       img_url,
-  //       title,
-  //       timestamp: formattedDate,
-  //       path,
-  //     };
-  //   });
+      return {
+        img_url,
+        title,
+        timestamp: formattedDate,
+        path,
+      };
+    });
 
-  //   const slicedArticles = formattedArticles.slice(0, 15);
-  //   setArticles1(slicedArticles);
-  //   setArticles2(formattedArticles.slice(15));
-  // }, [articles]);
+    const slicedArticles = formattedArticles.slice(0, 15);
+    setArticles1(slicedArticles);
+    setArticles2(formattedArticles.slice(15));
+  }, [articles]);
   return (
     <div>
       <Head>
@@ -50,34 +50,34 @@ export default function Home({ articles }) {
       </Head>
 
       <Navbar />
-      {/* <div className="px-10">
+      <div className="px-10">
         <Main articles={articles1} />
         <div className=" flex items-center justify-center">
           <div className="xs:max-w-[700px] max-w-[300px]">
             <Bottom articles={articles2} />
           </div>
-        </div> */}
-      {/* </div> */}
+        </div>
+      </div>
     </div>
   );
 }
 
-// export async function getServerSideProps() {
-//   // Fetch the articles from the API route
-//   const res = await fetch(`${process.env.LOCAL}/api/articles`);
-//   const articles = await res.json();
+export async function getServerSideProps() {
+  // Fetch the articles from the API route
+  const res = await fetch(`${process.env.LOCAL}/api/articles`);
+  const articles = await res.json();
 
-//   // Extract the desired properties from each article
-//   const filteredArticles = articles.map(({ img_url, title, timestamp, path }) => ({
-//     img_url,
-//     title,
-//     timestamp,
-//     path,
-//   }));
+  // Extract the desired properties from each article
+  const filteredArticles = articles.map(({ img_url, title, timestamp, path }) => ({
+    img_url,
+    title,
+    timestamp,
+    path,
+  }));
 
-//   return {
-//     props: {
-//       articles: filteredArticles,
-//     },
-//   };
-// }
+  return {
+    props: {
+      articles: filteredArticles,
+    },
+  };
+}
