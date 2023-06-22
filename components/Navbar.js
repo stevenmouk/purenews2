87bcdb36b28af08a11ba54adb43b38a2cddc7459
Steven Moukhametzianov <stevenmoukhametzianov@Stevens-MBP.home>
2023-06-22 16:48:22 -0400

    header change

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Playfair_Display, Bodoni_Moda } from "next/font/google";
 import Image from "next/image";
+import Link from "next/link";
 
 const playfair = Playfair_Display({ subsets: ["latin"], weight: "600" });
 const bodi = Bodoni_Moda({ subsets: ["latin"], weight: "600" });
@@ -8,13 +9,16 @@ const bodi = Bodoni_Moda({ subsets: ["latin"], weight: "600" });
 export default function Navbar() {
   return (
     <div
-      className={`${bodi.className} text-6xl w-full flex items-center sm:justify-center justify-staart pb-6  pt-8`}
+      className={`${bodi.className} sm:text-6xl text-4xl  w-full flex items-center sm:justify-center justify-staart pb-6  pt-8`}
     >
       {/* <Image className=" ml-8" src="/images/logo.svg" height="50" width="75"></Image> */}
 
-      <div className="text-[#e7131a] hidden sm:inline ">Wealthy Explorer</div>
-
-      <div className="text-[#e7131a] sm:hidden ml-3">WE</div>
+      <Link href="/" className="cursor-pointer">
+        <div className="text-[#e7131a] hidden sm:inline ">Wealthy Explorer</div>
+      </Link>
+      <Link href="/" className="cursor-pointer">
+        <div className="text-[#e7131a] sm:hidden ml-3 ">WE</div>
+      </Link>
       {/* <hr className="text-black" /> */}
     </div>
   );
