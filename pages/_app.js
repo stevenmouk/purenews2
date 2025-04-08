ecf850631bf298aa388414f77538e5235c1b21f7
Steven Mouk <70982072+stevenmouk@users.noreply.github.com>
2025-04-07 20:29:16 -0400

    weeee

--- pages/_app.js
+++ pages/_app.js
@@ -25,6 +25,15 @@ export default function App({ Component, pageProps }) {
         crossorigin="anonymous"
       ></script>
 
+      <Script id="clarity-script" strategy="afterInteractive">
+        {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "r0ocgbhk3i");
+          `}
+      </Script>
       <Component {...pageProps} />
       <Footer />
     </>
