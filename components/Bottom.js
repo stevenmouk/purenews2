b5c9f368589e63f9f43e27417ea290438fcb3b88
Steven Moukhametzianov <70982072+stevenmouk@users.noreply.github.com>
2025-04-11 12:57:07 -0400

    wee

--- components/Bottom.js
+++ components/Bottom.js
@@ -3,7 +3,11 @@ import Link from "next/link";
 
 export default function Bottom({ articles }) {
   return (
-    <section data-testid="homepage-latest" data-event-module="latest" className="snipcss-Y1qyP">
+    <section
+      data-testid="homepage-latest"
+      data-event-module="latest"
+      className="snipcss-Y1qyP"
+    >
       <div className="SectionHeading_root__JTxgB SectionHeading_grid__q2NPO">
         <h3 className="SectionHeading_heading__WZS7g">More News</h3>
         {/* <a
@@ -16,10 +20,17 @@ export default function Bottom({ articles }) {
       </div>
       <div className="Latest_ol__DrsDK">
         {articles?.map((article) => {
-          console.log(article);
+          // console.log(article);
           return (
-            <Link href={`/articles/${article?.slug}`} className="cursor-pointer" key={article?._id}>
-              <div className="Latest_li__5pluN" key={article?.frontmatter?.header_title}>
+            <Link
+              href={`/articles/${article?.slug}`}
+              className="cursor-pointer"
+              key={article?._id}
+            >
+              <div
+                className="Latest_li__5pluN"
+                key={article?.frontmatter?.header_title}
+              >
                 <article className="Latest_article__j7aws">
                   <div className="relative w-[150px]  sm:w-[200px] h-[100px] sm:h-[130px] mr-3">
                     {article?.frontmatter?.page_image_src_rel ? (
@@ -35,7 +46,10 @@ export default function Bottom({ articles }) {
                   </div>
 
                   <div className="Latest_articleInfo__8fUE8 w-[250px] sm:w-[350px] md:w-[450px] ">
-                    <h3 className="Latest_title__HB7uF" data-event-element="title">
+                    <h3
+                      className="Latest_title__HB7uF"
+                      data-event-element="title"
+                    >
                       {article?.frontmatter?.header_title}
                     </h3>
 
