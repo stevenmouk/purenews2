bf2185706c77b113edaa68f053775da432d8fa4d
Steven Moukhametzianov <70982072+stevenmouk@users.noreply.github.com>
2025-07-14 19:54:05 -0400

    wee

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
-import Image from "next/image";
-import { Archivo } from "next/font/google";
-import Navbar from "@/components/Navbar";
-import Link from "next/link";
-import Main from "@/components/Main";
 import Bottom from "@/components/Bottom";
-import { useEffect, useState } from "react";
-import Head from "next/head";
+import Main from "@/components/Main";
+import Navbar from "@/components/Navbar";
 import fs from "fs";
-import path from "path";
 import matter from "gray-matter";
+import { Archivo } from "next/font/google";
+import Head from "next/head";
+import path from "path";
 
 const archivo = Archivo({ subsets: ["latin"], weight: "800" });
 
@@ -20,10 +17,19 @@ export default function Home({ posts }) {
   return (
     <div>
       <Head>
-        <link rel="icon" type="image/png" href="/fave_pac/favicon-96x96.png" sizes="96x96" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/fave_pac/favicon-96x96.png"
+          sizes="96x96"
+        />
         <link rel="icon" type="image/svg+xml" href="/fave_pac/favicon.svg" />
         <link rel="shortcut icon" href="/fave_pac/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/fave_pac/apple-touch-icon.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/fave_pac/apple-touch-icon.png"
+        />
         <link rel="manifest" href="/fave_pac/site.webmanifest" />
         <meta name="google-adsense-account" content="ca-pub-5014630772635761" />
 
@@ -32,6 +38,10 @@ export default function Home({ posts }) {
           name="description"
           content="Stay informed with The Manhattan Times, your trusted source for the latest news. Explore a diverse range of articles covering stocks, markets, tech, personal finance, and more. Our expert analysis and in-depth reporting will keep you up-to-date with the ever-changing world of finance."
         />
+        <script
+          type="text/javascript"
+          src="http://classic.avantlink.com/affiliate_app_confirm.php?mode=js&authResponse=319fd5771af9cff8bcb1a96167a40b3de15e3152"
+        ></script>
       </Head>
 
       <Navbar />
@@ -53,7 +63,10 @@ export async function getStaticProps() {
   const posts = files.map((filename) => {
     const slug = filename.replace(".md", "");
 
-    const markdownWithMeta = fs.readFileSync(path.join("post", filename), "utf-8");
+    const markdownWithMeta = fs.readFileSync(
+      path.join("post", filename),
+      "utf-8"
+    );
 
     const { data: frontmatter } = matter(markdownWithMeta);
 
